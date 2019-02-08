2f713b63941fdcd6e695b3070b081cea5ff010e5
Booknerd2000 <denis.ajuah.2021@student.lhschools.org>
2019-02-07 21:04:13 -0500

    Added third step and initialized the variable total

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -7,16 +7,16 @@ public class Main {
     public static void main(String[] args) {
 
         int numberFromOneToNine;
-        //int numberBetween1768And1769;
+        int numberBetween1768And1769;
         //int yearOfBirth;
+        int total;
 
         System.out.println("I have a magic trick that you can use to impress your friends.\nLet me show you how it is done.");
         System.out.println("--------------------------------------------------------------");
         System.out.println("                                                              ");
+
         numberFromOneToNine = 4;
-
        // numberBetween1768And1769 = 1769;
-
         //yearOfBirth = 2003;
 
         System.out.println("First, ask someone to pick a number from 1 to 9.");
@@ -24,5 +24,9 @@ public class Main {
         System.out.println(" ");
         System.out.println("The next thing to do is to add that number by 2, \nmultiply it by 5, and add it by 50 in that exact order.");
         System.out.println("Doing that gave me "+((numberFromOneToNine+2)*5+50)+" as my number.");
+        total = 80;
+        System.out.println(" ");
+        System.out.println("After that, ask if their birthday has occurred or not. \nIf not, add 1768, and if so, add 1769");
+        System.out.println("My birthday has occurred, so I would add 1769 to get"+(total+1769));
     }
 }
