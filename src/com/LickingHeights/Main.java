33b91bf827abd0963eb68cfc44188f95b923b3fd
Booknerd2000 <denis.ajuah.2021@student.lhschools.org>
2019-02-07 19:59:58 -0500

    Input introduction

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -14,6 +14,8 @@ public class Main {
 
         keyboard = new Scanner(System.in)
 
+        System.out.println("I have a magic trick that you can use to impress your friends.\n First let me show you how it is done.");
+
         numberFromOneToNine = keyboard.nextLine();
 
         numberBetween1768And1769 = keyboard.nextLine();
