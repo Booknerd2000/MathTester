5d8b7505d5e059f65b6ab51d0015d45d9679ee3b
Booknerd2000 <denis.ajuah.2021@student.lhschools.org>
2019-02-07 20:18:43 -0500

    Removed user input

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -6,13 +6,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner keyboard;
-
         int numberFromOneToNine;
         int numberBetween1768And1769;
         int yearOfBirth;
-
-        keyboard = new Scanner(System.in)
 
         System.out.println("I have a magic trick that you can use to impress your friends.\n First let me show you how it is done.");
 
