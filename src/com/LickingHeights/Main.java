93bde0b82bfceaef613d15679597a12b534c8547
Booknerd2000 <denis.ajuah.2021@student.lhschools.org>
2019-02-07 19:42:00 -0500

    Set up int

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,8 +1,14 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int numberFromOneToNine;
+        int numberBetween1768And1769;
+        int yearOfBirth;
+        
     }
 }
