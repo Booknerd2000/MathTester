c9eba35a5e5f073f63bcfc30ff23eff2e146d7b6
Booknerd2000 <denis.ajuah.2021@student.lhschools.org>
2019-02-07 19:44:09 -0500

    Added user input

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -6,9 +6,12 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Scanner keyboard;
+
         int numberFromOneToNine;
         int numberBetween1768And1769;
         int yearOfBirth;
-        
+
+        keyboard = new Scanner(System.in)
     }
 }
