0ff2d7790de47d6436125de1ffc4efe51692a84e
Booknerd2000 <denis.ajuah.2021@student.lhschools.org>
2019-02-07 20:23:17 -0500

    Edited introduction

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -10,12 +10,14 @@ public class Main {
         int numberBetween1768And1769;
         int yearOfBirth;
 
-        System.out.println("I have a magic trick that you can use to impress your friends.\n First let me show you how it is done.");
+        System.out.println("I have a magic trick that you can use to impress your friends.\n Let me show you how it is done.");
 
-        numberFromOneToNine = keyboard.nextLine();
+        numberFromOneToNine = 4;
 
-        numberBetween1768And1769 = keyboard.nextLine();
+        numberBetween1768And1769 = 1769;
 
-        yearOfBirth = keyboard.nextLine();
+        yearOfBirth = 2003;
+
+        System.out.println("First ");
     }
 }
