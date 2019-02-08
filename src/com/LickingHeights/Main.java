59d6f7226e8b8c718dece335ec3d780b6b66420f
Booknerd2000 <denis.ajuah.2021@student.lhschools.org>
2019-02-07 20:42:41 -0500

    Explained the second step

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -7,18 +7,20 @@ public class Main {
     public static void main(String[] args) {
 
         int numberFromOneToNine;
-        int numberBetween1768And1769;
-        int yearOfBirth;
+        //int numberBetween1768And1769;
+        //int yearOfBirth;
 
-        System.out.println("I have a magic trick that you can use to impress your friends.\n Let me show you how it is done.");
+        System.out.println("I have a magic trick that you can use to impress your friends.\nLet me show you how it is done.");
 
         numberFromOneToNine = 4;
 
-        numberBetween1768And1769 = 1769;
+       // numberBetween1768And1769 = 1769;
 
-        yearOfBirth = 2003;
+        //yearOfBirth = 2003;
 
         System.out.println("First, ask someone to pick a number from 1 to 9.");
-        System.out.println("For this example, i'm going to use the number"+numberFromOneToNine);
+        System.out.println("For this example, i'm going to use the number "+numberFromOneToNine+".");
+        System.out.println("The next thing to do is to add that number by 2, \nmultiply it by 5, and add it by 50 in that exact order.");
+        System.out.println("Doing that gave me "+((numberFromOneToNine+2)*5+50));
     }
 }
